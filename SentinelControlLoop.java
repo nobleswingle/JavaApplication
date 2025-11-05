ce8fa76969049fa70f09865a8005b697973770d5
TheCodingNoble <noble.swingle14@gmail.com>
2025-11-05 10:24:22 +0100

    Java Files

---
+++ SentinelControlLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SentinelControlLoop{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int num;
+		int sum = 0;
+		int counter = 0;
+		
+		System.out.println("Enter -1 to terminate the loop");
+		while(true){
+			
+			System.out.print("Enter number: ");
+			num = input.nextInt();
+			
+			if(num == -1){
+				break;
+			}
+			sum += num;
+		}	
+		System.out.printf("The total of the %d number(s) is %d%n", counter, sum);
+	}
+}
